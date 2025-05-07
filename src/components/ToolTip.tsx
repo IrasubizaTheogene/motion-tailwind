26942bce72b80b4469017004a04269b7e2e27c6d
irasubiza theogene <irasubizatheogene86@gmail.com>
2025-05-07 23:38:11 +0100

    first commit

--- src/components/ToolTip.tsx
+++ src/components/ToolTip.tsx
@@ -1,27 +1,37 @@
-import { motion } from "framer-motion"
+import { AnimatePresence, motion } from "framer-motion"
 import { useState } from "react"
 
 function ToolTip() {
     let [show, setShow] = useState(true)
     let toolTipVar = {
-        hidden: { opacity: 0, y: -100 },
-        visible: { opacity: 1, y: -100 }
+        hidden: { opacity: 0, y: -15},
+        visible: { opacity: 1, y: 0}
     }
   return (
-    <div className="relative">
-        <button onMouseOver={() => setShow(true)} onMouseOut={() => setShow(false)}>hover Over me</button>
-
-    { show ? 
+    <div className="relative inline-block">
+        <button 
+        className="py-4 bg-blue-700 my-1 px-1 rounded text-blue-100"
+        onMouseEnter={() => setShow(true)}
+         onMouseLeave={() => setShow(false)}
+         >hover Over me</button>
+<AnimatePresence>
+    { show &&
         <motion.div
             variants={toolTipVar}
-            initial="visible"
-            animate="visible"
-            exit="hidden"
-            className="absolute bg-gray-700 text-gray-100"
+            initial="hidden"
+            animate='visible'
+            exit='hidden'
+            transition={{
+                duration: 0.1
+            }}
+            onMouseEnter={() => setShow(true)}
+            onMouseLeave={() => setShow(false)}
+            className="absolute bg-gray-700 text-gray-100 mt-2 p-2 rounded w-full text-center"
         >
-
+                Tool Tip Text
         </motion.div>
-    : ""}
+    }
+    </AnimatePresence>
     </div>
   )
 }
