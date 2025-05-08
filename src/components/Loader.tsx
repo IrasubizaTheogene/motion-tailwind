42fcc62c501e43f570f2702ff8171a778c725e11
irasubiza theogene <irasubizatheogene86@gmail.com>
2025-05-08 22:40:48 +0100

    Loader Commit

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -4,7 +4,7 @@ function Loader() {
   return (
     <div className="relative h-30 w-30 rounded-full border-4 border-blue-600">
         <motion.div 
-        animate={{ opacity: [1, 0.5, 1], rotateY: 180 }}
+        animate={{ opacity: [1, 0.5, 1], rotate: 360 }}
         style={{ borderTopColor: "transparent" }}
         transition={{ repeat: Infinity, ease: "linear" , duration: 1}}
         className="absolute inset-0 rounded-full border-blue-300 border-solid border-6"></motion.div>
